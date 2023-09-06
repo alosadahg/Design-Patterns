f32fef27b32f95af88983ab36337d4d4b41eea89
Hernah Grace Alosada <89090693+alosadahg@users.noreply.github.com>
2023-09-06 17:59:12 +0800

    Delete filter_pattern directory

--- filter_pattern/filter_criteria/Criteria.java
+++
@@ -1,9 +0,0 @@
-package filter_pattern.filter_criteria;
-
-import filter_pattern.Anime;
-
-import java.util.List;
-
-public interface Criteria {
-    List<Anime> meetsCriteria(List<Anime> animeList);
-}
